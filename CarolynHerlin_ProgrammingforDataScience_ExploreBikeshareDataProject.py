c02860bd9f9b7559a33710615351666911aef447
Carolyn Herlin <carolynherlin@comcast.net>
2019-07-16 11:35:05 -0600

    Update documentation for trip duration stats

--- CarolynHerlin_ProgrammingforDataScience_ExploreBikeshareDataProject.py
+++ CarolynHerlin_ProgrammingforDataScience_ExploreBikeshareDataProject.py
@@ -192,7 +192,7 @@ def station_stats(df):
  
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays statistics on the total and average trip duration as well as the longest and shortest trip."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
